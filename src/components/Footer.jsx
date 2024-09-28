6ed0cf01094f396fdeb5d016a0db1d50c22b1f45
gmontenegroBS <gmontenegro@blackstallion.com.ar>
2024-09-28 20:17:06 -0300

    carrousel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,72 @@
-export default function Footer  () {
-    return (
-      <footer className="bg-black text-gray-100 p-5 md:p-20">
-        <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
-          <div className="flex items-center mb-4 h-10">
-            <img src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390894/images/Logo_lkdzsg.png" alt="Logotipo de Paradigma Seeds" className="h-10" />
-          </div>
-  
-          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 mb-4">
-            <li><a href="#nosotros" className="hover:text-blue-500 font-bold">Nosotros</a></li>
-            <li><a href="#servicios" className="hover:text-blue-500 font-bold">Servicios</a></li>
-            <li><a href="#contacto" className="hover:text-blue-500 font-bold">Contacto</a></li>
-          </ul>
-  
-          <div className="flex space-x-4">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24"><path d="M22.675 0h-21.35C.783 0 0 .783 0 1.775v20.45C0 23.217.783 24 1.775 24h21.45C23.217 24 24 23.217 24 22.225V1.775C24 .783 23.217 0 22.225 0zM12 24h-3.61v-9.2H6.59V12h1.81V9.6c0-1.8 1.04-2.8 2.72-2.8.78 0 1.52.06 2.17.1v2.49h-1.51c-1.18 0-1.41.56-1.41 1.39V12h2.96l-.39 2.8h-2.57V24H12z"/></svg>
-            </a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2.163c3.149 0 3.539.012 4.785.069 1.236.057 2.244.25 3.096.54.74.274 1.377.634 1.948 1.205.571.571.931 1.208 1.205 1.948.291.852.482 1.86.539 3.096.057 1.246.069 1.636.069 4.785s-.012 3.539-.069 4.785c-.057 1.236-.25 2.244-.54 3.096-.274.74-.634 1.377-1.205 1.948-.571.571-1.208.931-1.948 1.205-.852.291-1.86.482-3.096.539-1.246.057-1.636.069-4.785.069s-3.539-.012-4.785-.069c-1.236-.057-2.244-.25-3.096-.539-.74-.274-1.377-.634-1.948-1.205-.571-.571-.931-1.208-1.205-1.948-.291-.852-.482-1.86-.539-3.096C2.175 15.702 2.163 15.312 2.163 12s.012-3.539.069-4.785c.057-1.236.25-2.244.54-3.096.274-.74.634-1.377 1.205-1.948C3.875 2.497 4.512 2.037 5.252 1.763c.852-.291 1.86-.482 3.096-.539C8.461 2.175 8.851 2.163 12 2.163zm0-2.163C8.63 0 8.25.012 7.032.06c-1.265.053-2.42.236-3.528.505-1.067.261-2.064.63-2.94 1.122-1.353.82-2.34 1.84-3.159 3.159C.63 3.89.261 4.887.06 5.954-.012 7.25 0 7.63 0 12s.012 4.75.06 5.946c.201 1.067.57 2.064 1.122 2.94.82 1.353 1.84 2.34 3.159 3.159.876.492 1.873.86 2.94 1.122 1.108.269 2.263.452 3.528.505 1.218.048 1.608.06 4.785.06s3.539-.012 4.785-.06c1.265-.053 2.42-.236 3.528-.505 1.067-.261 2.064-.63 2.94-1.122 1.353-.82 2.34-1.84 3.159-3.159.492-.876.86-1.873 1.122-2.94.269-1.108.452-2.263.505-3.528.048-1.218.06-1.608.06-4.785s-.012-3.539-.06-4.785c-.053-1.265-.236-2.42-.505-3.528-.261-1.067-.63-2.064-1.122-2.94-.82-1.353-1.84-2.34-3.159-3.159-.876-.492-1.873-.86-2.94-1.122C14.42.236 13.265.053 12 0zm0 5.839A6.161 6.161 0 1 0 18.161 12 6.168 6.168 0 0 0 12 5.839zm0 10.321A4.162 4.162 0 1 1 16.162 12 4.169 4.169 0 0 1 12 16.16zm4.781-10.959a1.003 1.003 0 1 0 1.003 1.003 1.003 1.003 0 0 0-1.003-1.003z"/></svg>
-            </a>
-          </div>
+import React from "react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black text-gray-100 p-5 md:p-20">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        {/* Logo en móvil y escritorio */}
+        <div className="flex items-center justify-center md:justify-start mb-4 md:mb-0">
+          {/* Logo para móvil */}
+          <img
+            src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727392658/images/PS_simbolo_blanco_Preferente_o7ilqx.png"
+            alt="Logotipo de Paradigma Seeds móvil"
+            className="h-10 md:hidden" // Solo visible en móvil
+          />
+          {/* Logo para escritorio */}
+          <img
+            src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390894/images/Logo_lkdzsg.png"
+            alt="Logotipo de Paradigma Seeds escritorio"
+            className="hidden md:block h-14" // Solo visible en escritorio
+          />
         </div>
-  
-        <div className="text-center mt-4">
-          <p className="text-gray-400">© 2024 Paradigma Seeds. Todos los derechos reservados.</p>
+
+        {/* Redes sociales */}
+        <div className="flex space-x-4 mb-4 md:mb-0 md:order-2">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-white"
+          >
+            <FaFacebook className="h-6 w-6" />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-white"
+          >
+            <FaInstagram className="h-6 w-6" />
+          </a>
         </div>
-      </footer>
-    );
-  };
-  
+
+        {/* Links en móvil y escritorio */}
+        <ul className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-6 md:order-1">
+          <li>
+            <a href="#nosotros" className="hover:text-blue-500 font-bold">
+              Nosotros
+            </a>
+          </li>
+          <li>
+            <a href="#servicios" className="hover:text-blue-500 font-bold">
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="#contacto" className="hover:text-blue-500 font-bold">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Copyright en la parte inferior */}
+      <div className="text-center mt-4">
+        <p className="text-gray-400">
+          © 2024 Paradigma Seeds. Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+}
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,37 +1,94 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules"; // Ajuste en la importación
+import "swiper/css";
+import "swiper/css/navigation"; // Importar estilos de los botones de navegación
 
-export default function Nosotros  ()  {
-    return (
-      <section id="nosotros" className="bg-black text-white py-5 md:py-16 bg-cover bg-center px-6 md:px-0" style={{ backgroundImage: "url('https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390895/images/backgroundNosotros_whfzhn.png')" }}>
-        {/* Título centrado */}
-        <div className="text-center mb-8 flex flex-col justify-center items-center">
-          <div className="w-[65%]">
-            <h1 className="text-customYellow text-2xl md:text-4xl font-bold">Registrados en INASE Categoría A Criaderos</h1>
-            <p className="text-lg md:text-xl mt-4 font-normal">Fitomejoramiento, investigación e inscripción de nuevas variedades.</p>
-            <div className="flex justify-center items-center mt-2 md:mt-5">
-              <div className="h-[1px] md:h-[2px] w-[100%] md:w-[70%] bg-gray-400"></div>
-            </div>
-          </div>
-          <div className="text-center">
-            <h2 className="text-white text-4xl font-bold my-10">Nosotros</h2>
+export default function Nosotros() {
+  return (
+    <section
+      id="nosotros"
+      className="bg-black text-white py-5 md:py-16 bg-cover bg-center px-6 md:px-0"
+      style={{
+        backgroundImage:
+          "url('https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390895/images/backgroundNosotros_whfzhn.png')",
+      }}
+    >
+      {/* Título centrado */}
+      <div className="text-center mb-8 flex flex-col justify-center items-center">
+        <div className="w-[65%]">
+          <h1 className="text-customYellow text-2xl md:text-4xl font-bold">
+            Registrados en INASE Categoría A Criaderos
+          </h1>
+          <p className="text-lg md:text-xl mt-4 font-normal">
+            Fitomejoramiento, investigación e inscripción de nuevas variedades.
+          </p>
+          <div className="flex justify-center items-center mt-2 md:mt-5">
+            <div className="h-[1px] md:h-[2px] w-[100%] md:w-[70%] bg-gray-400"></div>
           </div>
         </div>
-  
-        {/* Contenedor con dos columnas (imagen y texto) */}
-        <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-20 md:gap-8">
-          {/* Imagen a la izquierda */}
-          <div className="w-full md:w-1/2 flex justify-center items-center">
-            <img src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390896/images/PSsinfondo3_wsim2y.png" alt="Descripción de la imagen" className="rounded-lg w-[95%] md:w-[60%] h-auto object-contain md:object-cover" />
-          </div>
-  
-          {/* Texto a la derecha */}
-          <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
+        <div className="text-center">
+          <h2 className="text-white text-4xl font-bold my-10">Nosotros</h2>
+        </div>
+      </div>
+
+      {/* Contenedor del carrusel y el texto */}
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
+        {/* Contenedor del carrusel */}
+        <div className="w-full md:w-1/2">
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            navigation={true} // Habilitar botones de navegación en escritorio
+            modules={[Navigation]}
+            className="w-full h-[400px] md:h-[500px]" // Ajuste de tamaño
+          >
+            <SwiperSlide>
+              <img
+                src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727390896/images/PSsinfondo3_wsim2y.png"
+                alt="Descripción de la imagen"
+                className="rounded-lg w-full h-auto object-cover"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727395792/images/carrousel2_usirp3.png"
+                alt="Imagen 2"
+                className="rounded-lg w-full h-auto object-cover"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727395815/images/carrousel1_kyosda.png"
+                alt="Imagen 3"
+                className="rounded-lg w-full h-auto object-cover"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+
+        {/* Texto a la derecha */}
+        <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
           <div className="md:w-[70%] flex-col space-y-10 justify-center items-center md:text-lg">
-            <p>Somos un banco de semillas de cannabis de Corrientes, <strong>con más de 15 años de experiencia en el cultivo del cannabis.</strong></p>
-            <p>Nos dedicamos a la investigación, desarrollo, fitomejoramiento genético y registro de diversas variedades de cannabis.</p>
-            <p>Brindamos servicios de asesoramiento en todo el proceso de cultivo, desde la elección de la genética hasta los cuidados en cada etapa: germinación, vegetativo, floración, almacenamiento y consumo responsable.</p>
-          </div>
+            <p>
+              Somos un banco de semillas de cannabis de Corrientes,{" "}
+              <strong>
+                con más de 15 años de experiencia en el cultivo del cannabis.
+              </strong>
+            </p>
+            <p>
+              Nos dedicamos a la investigación, desarrollo, fitomejoramiento
+              genético y registro de diversas variedades de cannabis.
+            </p>
+            <p>
+              Brindamos servicios de asesoramiento en todo el proceso de
+              cultivo, desde la elección de la genética hasta los cuidados en
+              cada etapa: germinación, vegetativo, floración, almacenamiento y
+              consumo responsable.
+            </p>
           </div>
         </div>
-      </section>
-    );
-  };
+      </div>
+    </section>
+  );
+}
