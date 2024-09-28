d0c1ea305ed1252c362e9abd145980565ffd316e
gmontenegroBS <gmontenegro@blackstallion.com.ar>
2024-09-28 20:43:31 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram,FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="bg-black text-gray-100 p-5 md:p-20">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+      <div className="container mx-auto flex flex-row md:flex-row items-center justify-around md:justify-between">
         {/* Logo en móvil y escritorio */}
-        <div className="flex items-center justify-center md:justify-start mb-4 md:mb-0">
+        <div id="mobile-divIzquierda" className="flex items-center justify-center md:justify-start mb-4 md:mb-0 ">
           {/* Logo para móvil */}
           <img
             src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1727392658/images/PS_simbolo_blanco_Preferente_o7ilqx.png"
@@ -22,9 +22,19 @@ export default function Footer() {
         </div>
 
         {/* Redes sociales */}
-        <div className="flex space-x-4 mb-4 md:mb-0 md:order-2">
+        <div id="mobile-divDerecha ">
+        <div className="flex space-x-4 mb-4 md:mb-0 md:order-2 justify-end ">
+        <a
+            href="https://ar.linkedin.com/in/paradigmaseeds"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-white"
+          >
+            <FaLinkedin className="h-6 w-6" />
+
+          </a>
           <a
-            href="https://www.facebook.com"
+            href="https://www.facebook.com/share/Cmwm3uQzcRhkHrFT/?mibextid=qi2Omg"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-white"
@@ -32,38 +42,40 @@ export default function Footer() {
             <FaFacebook className="h-6 w-6" />
           </a>
           <a
-            href="https://www.instagram.com"
+            href="https://www.instagram.com/paradigma.seeds/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-white"
           >
             <FaInstagram className="h-6 w-6" />
           </a>
+
         </div>
 
         {/* Links en móvil y escritorio */}
-        <ul className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-6 md:order-1">
+        <ul className="flex flex-row text-sm md:text-md md:flex-col items-center text-center justify-rounded gap-1   md:space-y-0 md:space-x-6 md:order-1">
           <li>
-            <a href="#nosotros" className="hover:text-blue-500 font-bold">
+            <a href="#nosotros" className="hover:text-blue-500 font-bold ">
               Nosotros
             </a>
           </li>
           <li>
-            <a href="#servicios" className="hover:text-blue-500 font-bold">
+            <a href="#servicios" className="hover:text-blue-500 font-bold ">
               Servicios
             </a>
           </li>
           <li>
-            <a href="#contacto" className="hover:text-blue-500 font-bold">
+            <a href="#contacto" className="hover:text-blue-500 font-bold ">
               Contacto
             </a>
           </li>
         </ul>
+        </div>
       </div>
 
       {/* Copyright en la parte inferior */}
       <div className="text-center mt-4">
-        <p className="text-gray-400">
+        <p className="text-gray-400 text-xs md:text-md">
           © 2024 Paradigma Seeds. Todos los derechos reservados.
         </p>
       </div>
